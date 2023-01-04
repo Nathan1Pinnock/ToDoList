3e9370185acbee3c69a5b04510783efa5e9dd022
Nathan1Pinnock <121531478+Nathan1Pinnock@users.noreply.github.com>
2023-01-04 18:03:09 +0000

    Add files via upload

---
+++ ToDoList.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+# Initialize list to store tasks
+tasks = []
+
+# Function to add a task
+def add_task(event):
+  task = task_input.get()
+  tasks.append(task)
+  update_list()
+
+# Function to delete a task
+def delete_task(event):
+  task = task_listbox.get(tk.ANCHOR)
+  tasks.remove(task)
+  update_list()
+
+# Function to update the listbox
+def update_list():
+  task_listbox.delete(0, "end")
+  for task in tasks:
+    task_listbox.insert("end", task)
+
+# Create main window
+window = tk.Tk()
+window.title("To-Do List")
+
+# Create widgets
+task_label = tk.Label(window, text="Task:")
+task_input = tk.Entry(window)
+add_button = tk.Button(window, text="Add")
+delete_button = tk.Button(window, text="Delete")
+task_listbox = tk.Listbox(window)
+
+# Add button click events
+add_button.bind("<Button-1>", add_task)
+delete_button.bind("<Button-1>", delete_task)
+
+# Add widgets to window
+task_label.pack()
+task_input.pack()
+add_button.pack()
+delete_button.pack()
+task_listbox.pack()
+
+# Run the main loop
+window.mainloop()
